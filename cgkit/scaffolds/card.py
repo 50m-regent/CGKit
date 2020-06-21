dea3dd307afae370ec91db51f8f98895e547899d
りーぜんと <len.hirata@gmail.com>
2020-06-21 12:53:11 +0900

    created card.py

--- cgkit/scaffolds/card.py
+++ cgkit/scaffolds/card.py
@@ -1,2 +1,14 @@
 class Card:
-    pass
+    def __init__(self, data=None):
+        if data == None:
+            self.id   = 0
+            self.name = 'unnamed'
+
+            self.status = {}
+
+        else:
+            self.id   = data.id
+            self.name = data.name
+
+            self.status = data.status
+    
