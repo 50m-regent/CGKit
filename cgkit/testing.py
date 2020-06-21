28d793d812d5d161d2742c19ec5b9cb83b6b86a6
りーぜんと <len.hirata@gmail.com>
2020-06-21 13:26:53 +0900

    renamed

--- cgkit/testing.py
+++ cgkit/testing.py
@@ -1,3 +1,10 @@
-from ..scaffolds.game import Game
-from ..scaffolds.player import Player
-from ..scaffolds.card import Card
+from data.player_data import PlayerData
+from scaffolds.game import Game
+from scaffolds.player import Player
+from scaffolds.card import Card
+
+if __name__ == '__main__':
+    player1 = Player()
+    player2 = Player()
+
+    game = Game(players=[player1, player2])
